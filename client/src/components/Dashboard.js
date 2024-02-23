1ee8eb7bc597f160423d34e76eacd711fed04c5c
sonvanie <sonvanie@gmail.com>
2024-02-23 02:10:13 -0300

    dashboard css

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,7 +2,14 @@ import React, { Fragment, useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import ProyectList from "./proyectList";
 import { jwtDecode } from 'jwt-decode'; 
+import './styles/Dashboard.css'
+import { MdOutlineDashboard } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { IoMdMenu } from "react-icons/io";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { FaTasks } from "react-icons/fa";
+import { AiOutlinePicCenter } from "react-icons/ai";
+import { FaCalendarAlt } from "react-icons/fa";
 
 const Dashboard = ({ isAuthenticated }) => {
   const [userInfo, setUserInfo] = useState({});
@@ -34,39 +41,80 @@ const Dashboard = ({ isAuthenticated }) => {
       fetchUserInfo();
     }
   }, [isAuthenticated]);
+  
+  const toggleSidebar = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+  };
+
   return (
-    <Box sx={{ bgcolor: 'white', height: '100vh', display: 'flex', flexDirection: 'column', width: '80vw' }}>
-  <Box sx={{ position: 'relative', display: 'flex' }}>
-    <Box sx={{ 
-      bgcolor: '#6A0DAD',
-      width: '10px',
-      height: '100%',
-      position: 'absolute',
-      left: 0,
-    }} />
-    <Box sx={{ 
-      bgcolor: '#E1AFF7', 
-      color: 'white',
-      padding: '30px',
-      borderBottomRightRadius: '10px', 
-      borderBottomLeftRadius: '10px', 
-      display: 'flex', 
-      justifyContent: 'space-between', 
-      alignItems: 'center',
-      flex: 1
-    }}>
-      <Typography variant="h5">Proyectos</Typography>
-      <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center' }}>
-        <CgProfile style={{ fontSize: '30px', marginRight: '8px', verticalAlign: 'middle' }} />
-        {userInfo.user_name}
-      </Typography>
+    <Box sx={{ display: 'flex', height: '100vh' }}>
+      <nav className="sidebar">
+      <div className="logo-menu">
+        <h2 className="logo">
+          <CgProfile style={{ fontSize: '30px', marginRight: '8px', verticalAlign: 'middle' }} /> 
+          <span className="username">{userInfo.user_name}</span>
+        </h2>
+          <i className="bx bx-menu toggle-btn" onClick={toggleSidebar}>
+            <IoMdMenu />
+          </i>
+      </div>
+        <ul className="list">
+          <li className="list-item active ">
+            <a href="#">
+              <i className='bx bx-grid-alt'>
+                <MdOutlineDashboard/>
+              </i>
+              <span className="link-name" style={{ '--i': 1 }}> Dashboard </span>
+            </a>
+          </li>
+
+          <li className="list-item ">
+            <a href="#">
+              <i className="bx bx-grid-alt">
+                <AiOutlineFundProjectionScreen />
+              </i>
+              <span className="link-name" style={{ '--i': 2 }}> Proyectos </span>
+            </a>
+          </li>
+
+          <li className="list-item">
+            <a href="#">
+              <i className="bx bx-grid-alt">
+                <FaTasks />
+              </i>
+              <span className="link-name" style={{ '--i': 3 }}> Tareas </span>
+            </a>
+          </li>
+
+          <li className="list-item">
+            <a href="#">
+            <i className="bx bx-grid-alt">
+              <AiOutlinePicCenter />
+              </i>
+              <span className="link-name" style={{ '--i': 4 }}> Epicas </span>
+            </a>
+          </li>
+
+          <li className="list-item">
+            <a href="#">
+            <i className="bx bx-grid-alt">
+              <FaCalendarAlt />
+              </i>
+              <span className="link-name" style={{ '--i': 5 }}> Calendario </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <Box sx={{ bgcolor: 'white', flex: 1, padding: '20px', }}>
+        <Typography variant="h5">Proyectos</Typography>
+        <Fragment>
+          <ProyectList isAuthenticated={isAuthenticated} />
+        </Fragment>
+      </Box>
     </Box>
-  </Box>
-  <Fragment>
-    <ProyectList isAuthenticated={isAuthenticated} />
-  </Fragment>
-</Box>
-  )
+  );
 }
 
 export default Dashboard;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import './styles/Login.css';
 import { MdOutlineEngineering } from "react-icons/md";
